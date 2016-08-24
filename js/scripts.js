2c72ce8383f0e44468a926136220ff898a524edb
Katy Henning and Kevin Johnston <katy@>
2016-08-24 10:17:49 -0700

    Fulfill first spec with properly formatted tables

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 // Bussiness logic
+var playersArray = []
 
+function Player(playerName, score) {
+  this.playerName= playerName;
+  this.score = score;
+  playersArray.push(this);
+}
 
 // UI logic
 $(document).ready(function() {
+  // Player name submit button
+  $("#playerNameSubmit").click(function(event) {
+    event.preventDefault();
+    var player1Name = $("#player1Name").val();
+    var player2Name = $("#player2Name").val();
+
+    var player1 = new Player(player1Name, 0)
+    var player2 = new Player(player2Name, 0)
+
+
+
+  });
 
 });
