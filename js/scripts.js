23efd2c884dcca215adf2dbd3b4cf6904c20a9cd
Katy Henning and Kevin Johnston <katy@>
2016-08-24 13:42:12 -0700

    Fulfill the fourth spec- stop the turn and empty turnRollArray if a 1 is rolled

--- js/scripts.js
+++ js/scripts.js
@@ -8,40 +8,44 @@ function Player(playerName, score) {
   playersArray.push(this);
 }
 
+
+
+
 Array.prototype.sum = function(playersArray) {
   return this.reduce(function(a,b) {return a + b});
 }
-// var rollSum = function(turnRollArray) {
-//   var total = 0
-//   turnRollArray.forEach(function(x){
-//     total + x;
-//   });
-//   return total;
-// }
-
-
 
 
 // UI logic
 $(document).ready(function() {
   // Player name submit button
-  $("#playerNameSubmit").click(function(event) {
+  $("form#playerNameForm").submit(function(event) {
     event.preventDefault();
+    // debugger;
     var player1Name = $("#player1Name").val();
     var player2Name = $("#player2Name").val();
-
-    var player1 = new Player(player1Name, 0)
-    var player2 = new Player(player2Name, 0)
+    var player1 = new Player(player1Name, 0);
+    var player2 = new Player(player2Name, 0);
 
   });
+
   $("#diceRoll").click(function(event) {
     event.preventDefault();
     $("#rollResult").show();
+    $(".showTurnTotal").show();
     var randomRoll = (1 + Math.floor(Math.random() * 6));
-    turnRollArray.push(randomRoll);
-    console.log(turnRollArray.sum());
     $("#rollResult").text(randomRoll);
+    if (randomRoll >= 2) {
+      turnRollArray.push(randomRoll);
+      $("#turnTotal").text(turnRollArray.sum());
+    } else {
+    alert("You lost this round! NO POINTS FOR YOU!");
+      turnRollArray = [];
+      $("#turnTotal").text(turnRollArray)
+    return;
+  }
   });
+
 
 
 });
